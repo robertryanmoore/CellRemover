d8b8e6fb06187955cbb605ab2b97ed80103065ef
robertryanmoore <moore.robert.ryan@gmail.com>
2019-10-01 10:08:31 +0200

    First commit

---
+++ RemoveCell/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace RemoveCell
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.cmbEnviroList = new System.Windows.Forms.ComboBox();
+            this.txbCellNum = new System.Windows.Forms.TextBox();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.lblInfo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // cmbEnviroList
+            // 
+            this.cmbEnviroList.AllowDrop = true;
+            this.cmbEnviroList.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbEnviroList.FormattingEnabled = true;
+            this.cmbEnviroList.Items.AddRange(new object[] {
+            "Lab",
+            "Dev",
+            "QA",
+            "Pre-Prod",
+            "Prod"});
+            this.cmbEnviroList.Location = new System.Drawing.Point(18, 18);
+            this.cmbEnviroList.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.cmbEnviroList.Name = "cmbEnviroList";
+            this.cmbEnviroList.Size = new System.Drawing.Size(180, 28);
+            this.cmbEnviroList.TabIndex = 0;
+            // 
+            // txbCellNum
+            // 
+            this.txbCellNum.Location = new System.Drawing.Point(210, 18);
+            this.txbCellNum.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txbCellNum.MaxLength = 15;
+            this.txbCellNum.Name = "txbCellNum";
+            this.txbCellNum.Size = new System.Drawing.Size(127, 26);
+            this.txbCellNum.TabIndex = 1;
+            this.txbCellNum.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(348, 18);
+            this.btnRemove.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(112, 35);
+            this.btnRemove.TabIndex = 2;
+            this.btnRemove.Text = "Remove";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // lblInfo
+            // 
+            this.lblInfo.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInfo.Location = new System.Drawing.Point(18, 66);
+            this.lblInfo.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.lblInfo.Name = "lblInfo";
+            this.lblInfo.Size = new System.Drawing.Size(214, 42);
+            this.lblInfo.TabIndex = 4;
+            this.lblInfo.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblInfo.Click += new System.EventHandler(this.lblInfo_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(470, 98);
+            this.Controls.Add(this.lblInfo);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.txbCellNum);
+            this.Controls.Add(this.cmbEnviroList);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(492, 154);
+            this.MinimumSize = new System.Drawing.Size(492, 154);
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Cell Remover - ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbEnviroList;
+        private System.Windows.Forms.TextBox txbCellNum;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.Label lblInfo;
+    }
+}
+
---
+++ RemoveCell/Form1.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Principal;
+using System.Xml;
+using System.Net;
+using System.IO;
+using Oracle.ManagedDataAccess;
+using Oracle.ManagedDataAccess.Client;
+
+namespace RemoveCell
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            String currUser = System.DirectoryServices.AccountManagement.UserPrincipal.Current.DisplayName;//Get logged in users' name, reqires network connection
+            this.Text +=  currUser; 
+            this.cmbEnviroList.SelectedIndex = 0;
+
+            var webRequest = WebRequest.Create(@"http:///UserData.txt"); //changed for security
+
+            string[] cellNum;
+
+            try
+            {
+                //this whole block here grabs the data file from sharepoint
+                webRequest.Credentials = System.Net.CredentialCache.DefaultNetworkCredentials; //else we get 401 unauthorized
+                using (var response = webRequest.GetResponse())
+                using (var content = response.GetResponseStream())
+                using (var reader = new StreamReader(content))
+                {
+                    var strContent = reader.ReadToEnd();
+                    cellNum = strContent.Split(',');
+                }
+
+                //forloop included in try because there wont be a length if the file wasn't assible
+                for (int i = 0; i < cellNum.Length; i++)
+                {
+                    if (cellNum[i].Contains(currUser))
+                    {
+                        txbCellNum.Text = cellNum[i].Substring(cellNum[i].LastIndexOf('+'));
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                //This catch is empty because the program will still work if we can't preload a number
+            }
+            
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            string selection = cmbEnviroList.Text;
+            string dbDetails = "";
+            bool isCanceled = false;
+
+
+            //connection strings removed for security
+            switch (selection)
+            {
+                case "Lab":
+
+                    dbDetails = "";//works
+
+                    break;
+
+                case "Dev":
+
+
+                    dbDetails = ""; //works
+
+                    break;
+
+                case "QA":
+                    dbDetails = ";";//get details
+
+                    break;
+
+                case "Pre-Prod":
+
+                   dbDetails = "";//works
+
+                    break;
+
+                case "Prod":
+
+                    DialogResult shouldProceed = MessageBox.Show("Purge cell number " + txbCellNum.Text + " from PRODUCTION?", "Warning", MessageBoxButtons.YesNo);
+                    if (shouldProceed == DialogResult.Yes)
+                    {
+                        dbDetails = "";
+                    }
+                    else {
+                        isCanceled = true;
+                    }
+
+                    break;
+            }
+
+            try
+            {
+                if (isCanceled) {
+                    return;
+                }
+                DB_Call(dbDetails, NumberCleaner(txbCellNum.Text));
+            }
+            catch (Exception ex){
+                lblInfo.Text = "";
+
+                MessageBox.Show("Application unable to connect.\n\n 1. Please check your network connection.\n" +
+                    "2. A database password may have changed, please contact an IAM Developer.","Something Went Wrong!");
+
+                
+            }
+        }
+
+        
+
+        protected void DB_Call(string connStr, string cellNumber) {
+            OracleConnection conn = new OracleConnection(connStr);
+
+            lblInfo.Text = "Connecting...";
+
+            conn.Open();
+
+            lblInfo.Text = "Connected";
+
+
+            // security risks have been removed
+            string sql = "UPDATE [table] SET [field] = ''"
+                + ", [field] = ''"
+                + ", [field] = ''"
+                + "  [field] = '" + cellNumber+"'";
+
+            OracleCommand cmd = new OracleCommand(sql, conn);
+            cmd.CommandType = CommandType.Text;
+
+            try
+            {
+                OracleDataReader dr = cmd.ExecuteReader();
+                dr.Read();
+
+                MessageBox.Show("Operation successful", "Notification");
+
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+
+
+
+            conn.Close();
+
+            lblInfo.Text = "Connection closed";
+        }
+
+        // formats the cell number correctly for the sql statement, so +27123456789, 0123456789, 012 345 6789 aall work
+        protected string NumberCleaner(string cellNum) {
+
+            char[] temp = cellNum.ToCharArray();
+
+            cellNum = "";
+
+            for(int i = 0; i < temp.Length; i++)
+            {
+                if (!temp[i].Equals(' ')) {
+                    cellNum = cellNum + temp[i];
+                }
+            }
+
+            cellNum = cellNum.Trim();
+
+            if (cellNum.ElementAt(0).Equals('0')) {
+                cellNum = cellNum.Remove(0, 1);
+                cellNum = "+27" + cellNum;
+            }
+
+            cellNum = cellNum.Trim();
+
+            return cellNum;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblInfo_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+           
+        }
+    }
+}
